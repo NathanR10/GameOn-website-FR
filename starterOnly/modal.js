d4e30eda132a9f9173593f4705d18d617c120a00
Nathan Rousselet <nathan@agence-webup.com>
2022-03-24 16:03:47 +0100

    Add(modal): close button is now working

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,57 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const modalBtnClose = document.querySelectorAll(".modal-btn-close")
+const formData = document.querySelectorAll(".formData")
+const inputFirstname = document.querySelector("#first")
+const inputLastname = document.querySelector("#last")
+const inputEmail = document.querySelector("#email")
+const inputBirthdate = document.querySelector("#birthdate")
+const inputQuantity = document.querySelector("#quantity")
+const inputLocation = document.querySelectorAll(".checkbox-input")
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+modalBtnClose.forEach((btn) => btn.addEventListener("click", launchModal))
+inputLocation.forEach((btn) => btn.addEventListener("click", onLocationChange))
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// Submit form
+var firstnameIsValid = false // 2 characters min
+var lastnameIsValid = false  // 2 characters min
+var emailIsValid = false     // mail need to be valid
+var quantityIsValid = false // an input is needed
+var locationIsValid = false // an input is needed
+
+function onFirstnameChange() {
+  inputFirstname.value.length > 1 ? firstnameIsValid = true : firstnameIsValid = false
+}
+function onLastnameChange() {
+  inputLastname.value.length > 1 ? lastnameIsValid = true : lastnameIsValid = false
+}
+function onEmailChange() {
+  const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,24})+$/
+  inputEmail.value.match(mailRegex) ? emailIsValid = true : emailIsValid = false
+}
+function onQuantityChange() {
+  inputQuantity.value.length > 0 ? quantityIsValid = true : quantityIsValid = false
+  inputQuantity.value < 0 && (inputQuantity.value = 0)
+}
+function onLocationChange() {
+  document.querySelector('input[name="location"]:checked').value ? locationIsValid = true : locationIsValid = false
 }
 
-
+// launch-close modal form
+var showModal = false
+function launchModal() {
+  showModal ? showModal = false : showModal = true
+  showModal ? modalbg.style.display = "block" : modalbg.style.display = "none"
+}
