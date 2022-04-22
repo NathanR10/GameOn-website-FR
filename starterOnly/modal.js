826863ac381a051ae8ec39237364251278efa698
Nathan Rousselet <nathan@agence-webup.com>
2022-04-22 10:30:17 +0200

    Fix(style): responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,13 +12,16 @@ const modalbg = document.querySelector(".bground")
 const modalBtn = document.querySelectorAll(".modal-btn")
 const modalBtnClose = document.querySelectorAll(".modal-btn-close")
 const formData = document.querySelectorAll(".formData")
-const inputFirstname = document.querySelector("#first")
-const inputLastname = document.querySelector("#last")
-const inputEmail = document.querySelector("#email")
-const inputBirthdate = document.querySelector("#birthdate")
-const inputQuantity = document.querySelector("#quantity")
-const inputLocation = document.querySelectorAll(".checkbox-input")
-const modalForm = document.querySelector("#modalForm")
+const inputFirstname = document.querySelector("#first") // formData()
+const inputLastname = document.querySelector("#last") // formData()
+const inputEmail = document.querySelector("#email") // formData()
+const inputBirthdate = document.querySelector("#birthdate") // formData()
+const inputQuantity = document.querySelector("#quantity") // formData()
+const inputLocation = document.querySelectorAll(".checkbox-input") // formData()
+const locationList = document.querySelector("#locationList") // formData()
+const inputCGU = document.querySelector("#checkbox1") // formData()
+const modalForm = document.querySelector("#modalForm") // formData()
+const modalResult = document.querySelector("#modalResult") // formData()
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
@@ -29,15 +32,19 @@ inputLocation.forEach((btn) => btn.addEventListener("click", onLocationChange))
 var firstnameIsValid = false // 2 characters min
 var lastnameIsValid = false  // 2 characters min
 var emailIsValid = false     // mail need to be valid
+var birthdateIsValid = false // date need to be valid
 var quantityIsValid = false // an input is needed
 var locationIsValid = false // an input is needed
+var CGUIsValid = true // CGUIsValid is needed
 var formValid = false // if all true
 
 function isFormValid() {
   if (firstnameIsValid &&
   lastnameIsValid &&
   emailIsValid &&
+  birthdateIsValid &&
   quantityIsValid &&
+  CGUIsValid &&
   locationIsValid) {
     formValid = true
   } else {
@@ -48,36 +55,78 @@ function isFormValid() {
 function formSuccess() {
   // modalBody
   if (formValid) {
-    modalForm.remove()
-    var element = document.createElement("p");
-    element.appendChild(document.createTextNode('Merci pour votre inscription'));
-    document.getElementById('modalBody').appendChild(element);
+    modalForm.style.display = "none"
+    modalResult.style.display = "flex"
+    modalResult.style.display = "flex"
   } else {
     console.log('Form invalide !')
+    firstnameIsValid ? inputFirstname.parentNode.setAttribute("data-error-visible", "false") : inputFirstname.parentNode.setAttribute("data-error-visible", "true")
+    lastnameIsValid ? inputLastname.parentNode.setAttribute("data-error-visible", "false") : inputLastname.parentNode.setAttribute("data-error-visible", "true")
+    emailIsValid ? inputEmail.parentNode.setAttribute("data-error-visible", "false") : inputEmail.parentNode.setAttribute("data-error-visible", "true")
+    birthdateIsValid ? inputBirthdate.parentNode.setAttribute("data-error-visible", "false") : inputBirthdate.parentNode.setAttribute("data-error-visible", "true")
+    quantityIsValid ? inputQuantity.parentNode.setAttribute("data-error-visible", "false") : inputQuantity.parentNode.setAttribute("data-error-visible", "true")
+    locationIsValid ? locationList.setAttribute("data-error-visible", "false") : locationList.setAttribute("data-error-visible", "true")
+    CGUIsValid ? inputCGU.parentNode.setAttribute("data-error-visible", "false") : inputCGU.parentNode.setAttribute("data-error-visible", "true")
   }
   return false
 }
 
 function onFirstnameChange() {
-  inputFirstname.value.length > 1 ? firstnameIsValid = true : firstnameIsValid = false
+  const firstnameRegex = /^([a-zA-Z\s\-À-ÖØ-öø-ÿ]{2,})+$/
+  inputFirstname.value.match(firstnameRegex) ? firstnameIsValid = true : firstnameIsValid = false
+  let parent = inputFirstname.parentNode
+  firstnameIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('prenom->' + firstnameIsValid) // to remove
   isFormValid()
+
+  // passer : data-error-visible -> true
+
 }
 function onLastnameChange() {
-  inputLastname.value.length > 1 ? lastnameIsValid = true : lastnameIsValid = false
+  const lastnameRegex = /^([a-zA-Z\s\-À-ÖØ-öø-ÿ]{2,})+$/
+  inputLastname.value.match(lastnameRegex) ? lastnameIsValid = true : lastnameIsValid = false
+  let parent = inputLastname.parentNode
+  lastnameIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('nom->' + lastnameIsValid) // to remove
   isFormValid()
 }
 function onEmailChange() {
   const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,24})+$/
   inputEmail.value.match(mailRegex) ? emailIsValid = true : emailIsValid = false
+  let parent = inputEmail.parentNode
+  emailIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('email->' + emailIsValid) // to remove
+  isFormValid()
+}
+function onBirthdateChange() {
+  inputBirthdate.value ? birthdateIsValid = true : birthdateIsValid = false
+  let parent = inputBirthdate.parentNode
+  birthdateIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('birthdate->' + birthdateIsValid) // to remove
   isFormValid()
 }
 function onQuantityChange() {
-  inputQuantity.value.length > 0 ? quantityIsValid = true : quantityIsValid = false
-  inputQuantity.value < 0 && (inputQuantity.value = 0)
+  const quantityRegex = /^[1-9][0-9]*/
+  inputQuantity.value.match(quantityRegex) || parseInt(inputQuantity.value) === 0 ? quantityIsValid = true : quantityIsValid = false
+  let parent = inputQuantity.parentNode
+  quantityIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('quantity->' + quantityIsValid) // to remove
   isFormValid()
 }
 function onLocationChange() {
+  if (!document.querySelector('input[name="location"]:checked')) {
+    return
+  }
   document.querySelector('input[name="location"]:checked').value ? locationIsValid = true : locationIsValid = false
+  locationIsValid ? locationList.setAttribute("data-error-visible", "false") : locationList.setAttribute("data-error-visible", "true")
+  console.log('location->' + locationIsValid) // to remove
+  isFormValid()
+}
+function onCGUChange() {
+  inputCGU.checked ? CGUIsValid = true : CGUIsValid = false
+  let parent = inputCGU.parentNode
+  CGUIsValid ? parent.setAttribute("data-error-visible", "false") : parent.setAttribute("data-error-visible", "true")
+  console.log('CGU->' + locationIsValid) // to remove
   isFormValid()
 }
 
@@ -85,5 +134,6 @@ function onLocationChange() {
 var showModal = false
 function launchModal() {
   showModal ? showModal = false : showModal = true
-  showModal ? modalbg.style.display = "block" : modalbg.style.display = "none"
+  showModal ? modalbg.style.display = "flex" : modalbg.style.display = "none"
+  showModal ? document.body.style.overflow = "hidden" : document.body.style.overflow = ""
 }
